61ad4fd939230ccde5810d002edf1f64734fb6c6
Zachary ElledgeMCGhee <zte18a@acu.edu>
2020-09-20 18:07:23 -0500

    bug fixin

--- app/src/main/java/com/example/tictactoex/MainActivity.java
+++ app/src/main/java/com/example/tictactoex/MainActivity.java
@@ -1,25 +1,21 @@
 package com.example.tictactoex;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button[][] buttons=new Button[3][3];
-
     private boolean player1Turn = true;
-
     private int roundCount;
-
     private int player1points;
-
     private int player2points;
-     private TextView textViewPlater1;
-  private TextView textViewPlater2;
+    private TextView textViewPlater1;
+    private TextView textViewPlater2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,24 +62,84 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         roundCount++;
 
-    }
-
-    private boolean checkForWin(){
-        String[][] field = new String[3][3];
-
-        for (int i=0; i<3;i++)
-        {
-            for(int j =0; j<3;j++){
-                field[i][j]=buttons[i][j].getText().toString();
+        if(checkForWin()){
+            if(player1Turn){
+                player1Wins();
+            }
+            else{
+                player2Wins();
             }
         }
-        for(int i=0;i<3;i++){
-             if(field[i][0].equals(field[i][1])
-                && field[i][0].equals(field[i][2])
-                && field[i][0].equals(""))
-            {
-                return true;
-            } }}}}}
+        else if (roundCount == 9){
+            draw();
 
+        }
+        else {
+            player1Turn =!player1Turn;
+        }
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && field[i][0].equals("")) {
+                return true;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && field[0][i].equals("")) {
+                return true;
+            }
+        }
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && field[0][0].equals("")) {
+            return true;
+        }
+        return field[0][2].equals(field[1][1])
+                && field[0][0].equals(field[2][0])
+                && field[0][2].equals("");
+
+    }
+    private void player1Wins(){
+        player1points++;
+        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_LONG).show();
+        updatePointsText();
+        resetBoard();
+    }
+    private void player2Wins(){
+        player2points++;
+        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_LONG).show();
+        updatePointsText();
+        resetBoard();
+    }
+    private void draw(){
+        Toast.makeText(this, "Draw!", Toast.LENGTH_LONG).show();
+        resetBoard();
+    }
+    private void updatePointsText(){
+        textViewPlater1.setText("Player 1: "+player1points);
+        textViewPlater2.setText("Player 2: "+player2points);
+    }
+    private void resetBoard(){
+        for(int i=0;i<3; i++)
+        {
+            for(int j=0; j<3; j++){
+                buttons[i][j].setText("");
+            }
+        }
+        roundCount=0;
+        player1Turn = true;
+    }
 
 }
